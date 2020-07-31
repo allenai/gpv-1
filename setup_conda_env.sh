a78f0b641441dc5f53b332783d9f7f5fd25f88d7
Tanmay Gupta <tanmayg@vision-server13.corp.ai2>
2020-07-30 17:30:57 -0700

    update setup_conda_env.sh

---
+++ setup_conda_env.sh
@@ -0,0 +1,16 @@
+#conda create -n gpv python=3.6 -y
+#conda activate gpv
+
+################################################################################
+# conda installs
+################################################################################
+# conda install pytorch torchvision cudatoolkit=10.2 -c pytorch -y
+# conda install -c conda-forge scikit-image -y
+
+
+################################################################################
+# conda installs
+################################################################################
+# pip install hydra-core --upgrade --pre
+# pip install ipython
+pip install ipdb
