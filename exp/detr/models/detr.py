4b99eda709b095955f659c2e82e4a9b95372d1f3
Tanmay Gupta <tanmayg@vision-server13.corp.ai2>
2020-08-11 15:36:33 -0700

    add detr files

--- exp/detr/models/detr.py
+++ exp/detr/models/detr.py
@@ -1,49 +1,108 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+"""
+DETR model and criterion classes.
+"""
 import torch
+import torch.nn.functional as F
 from torch import nn
-from torchvision.models import resnet50
+
+import utils.box_ops as box_ops
+from utils.detr_misc import (NestedTensor, nested_tensor_from_tensor_list,
+                       accuracy, get_world_size, interpolate,
+                       is_dist_avail_and_initialized)
+
+from .backbone import build_backbone
+from .transformer import build_transformer
+from utils.matcher import build_matcher
+
 
 class DETR(nn.Module):
-    def __init__(
-            self, 
-            nqueries,
-            num_classes, 
-            hidden_dim, 
-            nheads,
-            num_encoder_layers, 
-            num_decoder_layers):
+    """ This is the DETR module that performs object detection """
+    def __init__(self, backbone, transformer, num_classes, num_queries, aux_loss=False):
+        """ Initializes the model.
+        Parameters:
+            backbone: torch module of the backbone to be used. See backbone.py
+            transformer: torch module of the transformer architecture. See transformer.py
+            num_classes: number of object classes
+            num_queries: number of object queries, ie detection slot. This is the maximal number of objects
+                         DETR can detect in a single image. For COCO, we recommend 100 queries.
+            aux_loss: True if auxiliary decoding losses (loss at each decoder layer) are to be used.
+        """
         super().__init__()
-        # We take only convolutional layers from ResNet-50 model
-        self.backbone = nn.Sequential(
-            *list(resnet50(pretrained=True).children())[:-2])
-        self.conv = nn.Conv2d(2048, hidden_dim, 1)
-        self.transformer = nn.Transformer(
-            hidden_dim, 
-            nheads,
-            num_encoder_layers, 
-            num_decoder_layers)
-        self.linear_class = nn.Linear(hidden_dim, num_classes)
-        self.linear_bbox = nn.Linear(hidden_dim, 4)
-        self.query_pos = nn.Parameter(torch.rand(nqueries, hidden_dim))
-        self.row_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
-        self.col_embed = nn.Parameter(torch.rand(50, hidden_dim // 2))
+        self.num_queries = num_queries
+        self.transformer = transformer
+        hidden_dim = transformer.d_model
+        self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
+        self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
+        self.backbone = backbone
+        self.aux_loss = aux_loss
 
-    def forward(self, inputs):
-        x = self.backbone(inputs)
-        h = self.conv(x) # BxCxHxW
-        B = h.shape[0]
-        H, W = h.shape[-2:]
-        pos = torch.cat([
-            self.col_embed[:W].unsqueeze(0).repeat(H, 1, 1), # HxWxC/2
-            self.row_embed[:H].unsqueeze(1).repeat(1, W, 1), # HxWxC/2
-            ], dim=-1).flatten(0, 1).unsqueeze(1) # HWx1xC
-        h = self.transformer(
-            pos + h.flatten(2).permute(2, 0, 1),    # encoder input: HWxBxC
-            self.query_pos.unsqueeze(1).repeat(1,B,1))  # decoder input: nqueriesx1xC
-        return self.linear_class(h), self.linear_bbox(h).sigmoid()
+    def forward(self, samples: NestedTensor):
+        """ The forward expects a NestedTensor, which consists of:
+               - samples.tensor: batched images, of shape [batch_size x 3 x H x W]
+               - samples.mask: a binary mask of shape [batch_size x H x W], containing 1 on padded pixels
+            It returns a dict with the following elements:
+               - "pred_logits": the classification logits (including no-object) for all queries.
+                                Shape= [batch_size x num_queries x (num_classes + 1)]
+               - "pred_boxes": The normalized boxes coordinates for all queries, represented as
+                               (center_x, center_y, height, width). These values are normalized in [0, 1],
+                               relative to the size of each individual image (disregarding possible padding).
+                               See PostProcess for information on how to retrieve the unnormalized bounding box.
+               - "aux_outputs": Optional, only returned when auxilary losses are activated. It is a list of
+                                dictionnaries containing the two above keys for each decoder layer.
+        """
+        if isinstance(samples, (list, torch.Tensor)):
+            samples = nested_tensor_from_tensor_list(samples)
+            
+        features, pos = self.backbone(samples)
+
+        src, mask = features[-1].decompose()
+        assert mask is not None
+        hs = self.transformer(self.input_proj(src), mask, self.query_embed.weight, pos[-1])[0]
+
+        outputs_class = self.class_embed(hs)
+        outputs_coord = self.bbox_embed(hs).sigmoid()
+        out = {'pred_logits': outputs_class[-1], 'pred_boxes': outputs_coord[-1]}
+        if self.aux_loss:
+            out['aux_outputs'] = self._set_aux_loss(outputs_class, outputs_coord)
+        return out
+
+    @torch.jit.unused
+    def _set_aux_loss(self, outputs_class, outputs_coord):
+        # this is a workaround to make torchscript happy, as torchscript
+        # doesn't support dictionary with non-homogeneous values, such
+        # as a dict having both a Tensor and a list.
+        return [{'pred_logits': a, 'pred_boxes': b}
+                for a, b in zip(outputs_class[:-1], outputs_coord[:-1])]
 
 
-if __name__=='__main__':
-    detr = DETR(8,2,10,2,3,3)
-    inputs = torch.randn([5,3,100,100])
-    logits, bbox = detr(inputs)
-    import ipdb; ipdb.set_trace()
+class MLP(nn.Module):
+    """ Very simple multi-layer perceptron (also called FFN)"""
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x):
+        for i, layer in enumerate(self.layers):
+            x = F.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
+
+
+def create_detr(cfg):
+    backbone = build_backbone(cfg)
+
+    transformer = build_transformer(cfg)
+
+    model = DETR(
+        backbone,
+        transformer,
+        num_classes=cfg.num_classes,
+        num_queries=cfg.num_queries,
+        aux_loss=cfg.aux_loss)
+    
+    return model
