7b3b04b9934453b76e94533e5f9483525179f6ab
Tanmay Gupta <tanmayg@vision-server13.corp.ai2>
2020-08-13 10:16:04 -0700

    train script with cktp, tb_logs and eval

--- exp/detr/train.py
+++ exp/detr/train.py
@@ -1,11 +1,13 @@
 import os
 import hydra
 import torch
+from torch.utils.tensorboard import SummaryWriter
 import itertools
 import numpy as np
 import skimage.io as skio
 
 from .models.detr import create_detr
+from .eval import eval_model
 from datasets.clevr_detection_train_task import ClevrDetectionTrainTask
 from utils.set_criterion import SetCriterion
 from utils.matcher import HungarianMatcher
@@ -13,7 +15,9 @@ from utils.bbox_utils import vis_bbox
 import utils.io as io
 
 
-def train_model(model,dataloader,cfg):
+def train_model(model,dataloaders,cfg):
+    writer = SummaryWriter(log_dir=cfg.tb_dir)
+
     backbone_params = [p for n, p in model.named_parameters() \
             if 'backbone' in n and p.requires_grad]
     other_params = [p for n, p in model.named_parameters() \
@@ -55,7 +59,7 @@ def train_model(model,dataloader,cfg):
 
     step = 0
     for epoch in range(cfg.training.num_epochs):
-        for it,data in enumerate(dataloader):
+        for it,data in enumerate(dataloaders['train']):
             imgs, targets = data
             imgs = imgs.to(torch.device(0))
             targets = [{k: v.cuda() for k, v in t.items()} for t in targets]
@@ -66,25 +70,9 @@ def train_model(model,dataloader,cfg):
             outputs = model(imgs)
             losses = set_criterion(outputs,targets)
             
-
             loss_to_optim = sum(
                 losses[k] * wts_dict[k] for k in losses.keys() if k in wts_dict)
             
-            if step%10==0:
-                loss_str = f'Epoch: {epoch} | Iter: {it} | Step: {step} | '
-                for k in itertools.chain(
-                        wts_dict.keys(),['class_error','cardinality_error']):
-                    loss_value = round(losses[k].item(),4)
-                    loss_str += f'{k}: {loss_value} | '
-                print(loss_str)
-
-
-            if step%100==0:
-                with torch.no_grad():
-                    model.eval()
-                    visualize(model,dataloader,cfg,step)
-                    model.train()
-                    
             optimizer.zero_grad()
             loss_to_optim.backward()
             if cfg.training.clip_max_norm > 0:
@@ -92,15 +80,60 @@ def train_model(model,dataloader,cfg):
                     model.parameters(), 
                     cfg.training.clip_max_norm)
             optimizer.step()
+            
+            if step%cfg.training.log_step==0:
+                loss_str = f'Epoch: {epoch} | Iter: {it} | Step: {step} | '
+                writer.add_scalar('Epoch',epoch,step)
+                writer.add_scalar('Iter',it,step)
+                writer.add_scalar('Step',step,step)
+                writer.add_scalar(
+                    'Lr/all_except_backbone',
+                    lr_scheduler.get_last_lr()[0],
+                    step)
+                writer.add_scalar(
+                    'Lr/backbone',
+                    lr_scheduler.get_last_lr()[1],
+                    step)
+                for k in itertools.chain(
+                        wts_dict.keys(),['class_error','cardinality_error']):
+                    loss_value = round(losses[k].item(),4)
+                    loss_str += f'{k}: {loss_value} | '
+                    writer.add_scalar(f'Loss/{k}/train',loss_value,step)
+                print(loss_str)
+
+            if step%cfg.training.vis_step==0:
+                with torch.no_grad():
+                    model.eval()
+                    visualize(model,dataloaders['val'],cfg,step)
+                    
+            if step%cfg.training.ckpt_step==0:
+                torch.save({
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'epoch': epoch,
+                    'iter': it,
+                    'step': step,
+                    'lr': lr_scheduler.get_last_lr()
+                }, os.path.join(cfg.ckpt_dir,str(step).zfill(6)+'.pth'))
 
             step += 1
 
         lr_scheduler.step()
+        
+        if epoch%10==0:
+            with torch.no_grad():
+                model.eval()
+                AP = {}
+                for subset, dataloader in dataloaders.items():
+                    AP[subset] = eval_model(model,dataloaders[subset],cfg)[0]['AP']
+                    writer.add_scalar(f'AP/{subset}',AP[subset],epoch)
 
+                print('Epoch:',epoch,'AP',AP)
 
 def visualize(model,dataloader,cfg,step):
     vis_dir = os.path.join(cfg.exp_dir,'train_vis')
     io.mkdir_if_not_exists(vis_dir,recursive=True)
+    io.mkdir_if_not_exists(cfg.ckpt_dir,recursive=True)
 
     count = 0
     for data in dataloader:
@@ -108,8 +141,8 @@ def visualize(model,dataloader,cfg,step):
         imgs = imgs.to(torch.device(0))
 
         outputs = model(imgs)
-        imgs = 255*(0.5+0.25*imgs.tensors.permute(0,2,3,1).detach().cpu().numpy())
-        imgs = imgs.astype(np.uint8)
+        imgs = dataloader.dataset.get_images_from_tensor(imgs)
+        imgs = imgs.detach().cpu().numpy().astype(np.uint8)
 
         # visualize predictions
         pred_prob = outputs['pred_logits'].softmax(-1)
@@ -149,10 +182,17 @@ def main(cfg):
 
     model = create_detr(cfg.model.detr).cuda()
     
-    dataset = ClevrDetectionTrainTask(cfg.task.clevr_detection_train,'train')
-    dataloader = dataset.get_dataloader(batch_size=cfg.training.batch_size,num_workers=8)
+    datasets = {
+        'train': ClevrDetectionTrainTask(cfg.task.clevr_detection_train,'train'),
+        'val': ClevrDetectionTrainTask(cfg.task.clevr_detection_train,'val')}
+    
+    dataloaders = {}
+    for subset,dataset in datasets.items():
+        dataloaders[subset] = dataset.get_dataloader(
+            batch_size=cfg.training.batch_size,
+            num_workers=8)
 
-    train_model(model,dataloader,cfg)
+    train_model(model,dataloaders,cfg)
 
 
 if __name__=='__main__':
