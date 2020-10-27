5fda5b9ce103b3c4b33c89e30749485bbd174b78
Tanmay Gupta <tanmayg@vision-server13.corp.ai2>
2020-10-27 09:59:30 -0700

    scp to aws

--- data/scp_to_aws.sh
+++ data/scp_to_aws.sh
@@ -1,9 +1,10 @@
 hostname=172.16.0.172
-sshkey=/home/tanmayg/.ssh/allenai_mac
+sshkey=$1
 
 ## copy splits
 mkdir /home/tanmayg/Data/gpv/learning_phase_data
 scp -r -i $sshkey tanmayg@$hostname:/home/tanmayg/Data/gpv/learning_phase_data/splits.zip /home/tanmayg/Data/gpv/learning_phase_data
+unzip /home/tanmayg/Data/gpv/learning_phase_data/splits.zip -d /home/tanmayg/Data/gpv/learning_phase_data/
 
 ## copy detr
 detr_pth=/home/tanmayg/Data/gpv/coco_exp/detr-r50-e632da11.pth
