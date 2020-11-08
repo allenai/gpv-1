4cc6a849bdbb123e58860dc5c57dc21c8b5c3d61
Tanmay Gupta <tanmayg@tanmayg.vis.ai2>
2020-11-08 00:46:39 +0000

    linear answer head

--- exp/gpv_box_text_feats/models/answer_head.py
+++ exp/gpv_box_text_feats/models/answer_head.py
@@ -35,12 +35,44 @@ class AnswerHead(nn.Module):
         return torch.sum(vocab_classifiers*answer_embed,3) # LxBxTxV
 
 
+
+class LinearAnswerHead(nn.Module):
+    def __init__(
+            self,
+            vocab,
+            hidden_dim,
+            vocab_embed=None):
+        super().__init__()
+        self.vocab = vocab
+        
+        if vocab_embed is None:
+            vocab_embed = 0.1*torch.randn([len(self.vocab),hidden_dim])
+        else:
+            vocab_embed = torch.FloatTensor(vocab_embed)
+            print('Using precomputed vocab embeddings')
+        
+        self.vocab_embed = nn.Parameter(vocab_embed,requires_grad=False)
+        self.classifier = nn.Linear(
+            hidden_dim,
+            len(vocab))
+
+    def forward(self,answer_embed):
+        """
+        answer_embed: LxBxTxD
+        """
+        return self.classifier(answer_embed)
+
 def build_answer_head(cfg,classifier_transform):
     vocab_embed = None
     if cfg.vocab_embed is not None:
         vocab_embed = np.load(cfg.vocab_embed)
     
     vocab = io.load_json_object(cfg.vocab)
+    print('vocab len:',len(vocab))
+    if cfg.answer_head=='linear':
+        print('Using linear answer head')
+        return LinearAnswerHead(vocab,cfg.detr.hidden_dim,vocab_embed)
+
     return AnswerHead(
         vocab,
         cfg.detr.hidden_dim,
