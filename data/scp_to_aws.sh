d070ea8c7197e1b2c35cff9db0c6c3868fd27b21
Tanmay Gupta <tanmayg@tanmayg.vis.ai2>
2020-12-01 22:53:11 +0000

    add vg feat download

--- data/scp_to_aws.sh
+++ data/scp_to_aws.sh
@@ -15,3 +15,8 @@ scp -i $sshkey tanmayg@$hostname:$detr_pth /home/tanmayg/Data/gpv/coco_exp
 ckpt_pth=/home/tanmayg/Data/gpv/coco_exp/gpv/ckpts/model.pth
 mkdir -p /home/tanmayg/Data/gpv/coco_exp/gpv/ckpts
 scp -i $sshkey tanmayg@$hostname:$ckpt_pth /home/tanmayg/Data/gpv/coco_exp/gpv/ckpts/frozen_model.pth
+
+## copy vg det feats
+feat_pth=/home/tanmayg/Data/gpv/learning_phase_data/vg_det_feats/data.mdb
+mkdir -p /home/tanmayg/Data/gpv/learning_phase_data/vg_det_feats
+wget https://dl.fbaipublicfiles.com/vilbert-multi-task/datasets/coco/features_100/COCO_trainval_resnext152_faster_rcnn_genome.lmdb/data.mdb -P /home/tanmayg/Data/gpv/learning_phase_data/vg_det_feats
