304466ddd3d227f980535a4ed0ec387d573923e8
Tanmay Gupta <tanmayg@allenai.org>
2021-01-08 10:02:03 +0000

    update scripts

--- exp/gpv_biatt_box_text/scripts/evaluate_det_cap_frozen.sh
+++ exp/gpv_biatt_box_text/scripts/evaluate_det_cap_frozen.sh
@@ -1,8 +1,9 @@
 SUBSET=$1
 TASK=$2
 python -m exp.gpv_biatt_box_text.compute_predictions \
-    exp_name=gpv_biatt_det_vqa_freeze_then_ft \
+    exp_name=gpv_biatt_det_cap_freeze \
     learning_datasets=det \
+    model.max_text_len=20 \
     eval.task=$TASK \
     eval.subset=$SUBSET \
     eval.predict=True \
--- exp/gpv_biatt_box_text/scripts/evaluate_det_vqa_long_freeze_then_ft.sh
+++ exp/gpv_biatt_box_text/scripts/evaluate_det_vqa_long_freeze_then_ft.sh
@@ -1,7 +1,7 @@
 SUBSET=$1
 TASK=$2
 python -m exp.gpv_biatt_box_text.compute_predictions \
-    exp_name=gpv_biatt_det_vqa_freeze \
+    exp_name=gpv_biatt_det_vqa_long_freeze_then_ft \
     learning_datasets=det \
     eval.task=$TASK \
     eval.subset=$SUBSET \
