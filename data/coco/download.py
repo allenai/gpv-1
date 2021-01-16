23b3c0e00d6682372aa11fb8416ed7e4f3b3c879
Tanmay Gupta <tanmayg@allenai.org>
2021-01-16 07:34:24 -0800

    add option to download coco images only

--- data/coco/download.py
+++ data/coco/download.py
@@ -8,6 +8,21 @@ import utils.io as io
 @hydra.main(config_path='../../configs',config_name='data/download.yaml')
 def main(cfg):
     print(cfg.pretty())
+    if cfg.download_coco_images_only:
+        data_type_dir = os.path.join(cfg.exp_dir,'coco/images')
+        io.mkdir_if_not_exists(data_type_dir)
+        for url in cfg.urls['coco']['images'].values():
+            filename = url.split('/')[-1]
+            filepath = os.path.join(data_type_dir,filename)
+            print(filepath)
+            if not os.path.exists(filepath):
+                print(f'Downloading coco/images/{url}')
+                wget.download(url,out=data_type_dir)
+                print(' [Done]')
+                print(f'Extracting from {filepath}')
+                with ZipFile(filepath,'r') as zipobj:
+                    zipobj.extractall(data_type_dir)
+
     for dataset in ['coco','vqa']: #cfg.urls.keys():
         dataset_dir = os.path.join(cfg.exp_dir,dataset)
         for data_type in cfg.urls[dataset].keys():
