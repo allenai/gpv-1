0d65bfca578f49e07490dc79e3b9bdef51b1ce2c
Tanmay Gupta <tanmayg@allenai.org>
2021-01-17 10:40:56 +0000

    fix else

--- data/coco/download.py
+++ data/coco/download.py
@@ -5,12 +5,12 @@ from zipfile import ZipFile
 
 import utils.io as io
 
-@hydra.main(config_path='../../configs',config_name='data/download.yaml')
+@hydra.main(config_path='../../configs/data/',config_name='download.yaml')
 def main(cfg):
     print(cfg.pretty())
     if cfg.download_coco_images_only:
         data_type_dir = os.path.join(cfg.exp_dir,'coco/images')
-        io.mkdir_if_not_exists(data_type_dir)
+        io.mkdir_if_not_exists(data_type_dir,recursive=True)
         for url in cfg.urls['coco']['images'].values():
             filename = url.split('/')[-1]
             filepath = os.path.join(data_type_dir,filename)
@@ -22,23 +22,23 @@ def main(cfg):
                 print(f'Extracting from {filepath}')
                 with ZipFile(filepath,'r') as zipobj:
                     zipobj.extractall(data_type_dir)
-
-    for dataset in ['coco','vqa']: #cfg.urls.keys():
-        dataset_dir = os.path.join(cfg.exp_dir,dataset)
-        for data_type in cfg.urls[dataset].keys():
-            data_type_dir = os.path.join(dataset_dir,data_type)
-            io.mkdir_if_not_exists(data_type_dir,recursive=True)
-            for url in cfg.urls[dataset][data_type].values():
-                filename = url.split('/')[-1]
-                filepath = os.path.join(data_type_dir,filename)
-                print(filepath)
-                if not os.path.exists(filepath):
-                    print(f'Downloading {dataset}/{data_type}/{url}')
-                    wget.download(url,out=data_type_dir)
-                    print(' [Done]')
-                    print(f'Extracting from {filepath}')
-                    with ZipFile(filepath,'r') as zipobj:
-                        zipobj.extractall(data_type_dir)
+    else:
+        for dataset in ['coco','vqa']: #cfg.urls.keys():
+            dataset_dir = os.path.join(cfg.exp_dir,dataset)
+            for data_type in cfg.urls[dataset].keys():
+                data_type_dir = os.path.join(dataset_dir,data_type)
+                io.mkdir_if_not_exists(data_type_dir,recursive=True)
+                for url in cfg.urls[dataset][data_type].values():
+                    filename = url.split('/')[-1]
+                    filepath = os.path.join(data_type_dir,filename)
+                    print(filepath)
+                    if not os.path.exists(filepath):
+                        print(f'Downloading {dataset}/{data_type}/{url}')
+                        wget.download(url,out=data_type_dir)
+                        print(' [Done]')
+                        print(f'Extracting from {filepath}')
+                        with ZipFile(filepath,'r') as zipobj:
+                            zipobj.extractall(data_type_dir)
 
 
 
