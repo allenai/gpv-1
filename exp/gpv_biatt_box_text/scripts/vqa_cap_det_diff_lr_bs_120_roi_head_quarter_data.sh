7d359b7ee753bdc01ef3061dca805cd06c91e000
Tanmay Gupta <tanmayg@allenai.org>
2021-01-28 00:50:38 +0000

    script for quater data

---
+++ exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_bs_120_roi_head_quarter_data.sh
@@ -0,0 +1,18 @@
+python -m exp.gpv_biatt_box_text.train_distr \
+    exp_name=quarter_data_gpv_biatt_det_vqa_cap_wt_cap_5e-2_bs_120_roi_head_concat_roi_and_detr_hs \
+    ngpus_per_node=3 \
+    multiprocessing_distributed=True \
+    dist_url='tcp://localhost:10002' \
+    learning_datasets=det_vqa_cap \
+    training.ckpt=null \
+    training.freeze=True \
+    training.frozen_epochs=40 \
+    training.frozen_batch_size=120 \
+    training.batch_size=120 \
+    losses.CaptionLoss.loss_wts.loss_caption=5e-2 \
+    losses.VqaLoss.loss_wts.loss_vqa=1 \
+    model.roi_head=True \
+    model.detr_joiner.detr_dim=2304 \
+    task_configs.coco_captioning.max_samples=73233 \
+    task_configs.coco_detection.max_samples=43634 \
+    task_configs.coco_vqa.max_samples=84597
