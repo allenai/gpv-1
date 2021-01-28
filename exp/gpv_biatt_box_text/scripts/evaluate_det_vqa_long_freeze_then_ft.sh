9d9ab16b5b9b85451da95fd1963e9e6ad42d8cb7
Tanmay Gupta <tanmayg@allenai.org>
2021-01-28 20:25:16 +0000

    update scripts

--- exp/gpv_biatt_box_text/scripts/evaluate_det_vqa_long_freeze_then_ft.sh
+++
@@ -1,9 +0,0 @@
-SUBSET=$1
-TASK=$2
-python -m exp.gpv_biatt_box_text.compute_predictions \
-    exp_name=gpv_biatt_det_vqa_long_freeze_then_ft \
-    learning_datasets=det \
-    eval.task=$TASK \
-    eval.subset=$SUBSET \
-    eval.predict=True \
-    eval.num_eval_batches=null
--- exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_bs_120_quarter_data_roi_head.sh
+++ exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_bs_120_quarter_data_roi_head.sh
@@ -1,8 +1,8 @@
 python -m exp.gpv_biatt_box_text.train_distr \
     exp_name=quarter_data_gpv_biatt_det_vqa_cap_wt_cap_5e-2_bs_120_roi_head_concat_roi_and_detr_hs \
-    ngpus_per_node=3 \
+    ngpus_per_node=1 \
     multiprocessing_distributed=True \
-    dist_url='tcp://localhost:10002' \
+    dist_url='tcp://localhost:10001' \
     learning_datasets=det_vqa_cap \
     training.ckpt=null \
     training.freeze=True \
@@ -13,6 +13,6 @@ python -m exp.gpv_biatt_box_text.train_distr \
     losses.VqaLoss.loss_wts.loss_vqa=1 \
     model.roi_head=True \
     model.detr_joiner.detr_dim=2304 \
-    task_configs.coco_captioning.max_samples=73233 \
-    task_configs.coco_detection.max_samples=43634 \
-    task_configs.coco_vqa.max_samples=84597
+    task_configs.coco_captioning.max_samples.train=73233 \
+    task_configs.coco_detection.max_samples.train=43634 \
+    task_configs.coco_vqa.max_samples.train=84597
