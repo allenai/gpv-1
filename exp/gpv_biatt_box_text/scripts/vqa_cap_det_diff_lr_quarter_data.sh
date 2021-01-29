ae9142cfe91f1a26d80ad603d07daa5d5d07bd96
Tanmay Gupta <tanmayg@allenai.org>
2021-01-28 16:20:52 -0800

    update scripts

--- exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_quarter_data.sh
+++ exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_quarter_data.sh
@@ -1,15 +1,17 @@
-EXP_NAME=quarter_data_gpv_biatt_det_vqa_cap_wt_cap_5e-2_bs_120_freeze_then_ft
+EXP_NAME=quarter_data_gpv_biatt_det_vqa_cap_wt_cap_5e-2
 python -m exp.gpv_biatt_box_text.train_distr \
     exp_name=$EXP_NAME \
     ngpus_per_node=1 \
     multiprocessing_distributed=True \
-    dist_url='tcp://localhost:10003' \
+    dist_url='tcp://localhost:10001' \
     learning_datasets=det_vqa_cap \
     training.ckpt=null \
     training.freeze=True \
-    training.frozen_epochs=10 \
+    training.frozen_epochs=5 \
     training.frozen_batch_size=120 \
     training.batch_size=120 \
+    training.run_eval_at_launch=False \
+    training.run_vis_at_launch=False \
     losses.CaptionLoss.loss_wts.loss_caption=5e-2 \
     losses.VqaLoss.loss_wts.loss_vqa=1 \
     model.roi_head=False \
@@ -21,13 +23,17 @@ python -m exp.gpv_biatt_box_text.train_distr \
 ckpt="/home/tanmayg/Data/gpv/coco_exp/${EXP_NAME}/ckpts/model.pth"
 python -m exp.gpv_biatt_box_text.train_distr \
     exp_name=$EXP_NAME \
-    ngpus_per_node=2 \
+    ngpus_per_node=1 \
     multiprocessing_distributed=True \
-    dist_url='tcp://localhost:10003' \
+    dist_url='tcp://localhost:10001' \
     learning_datasets=det_vqa_cap \
     training.ckpt=$ckpt \
-    training.freeze=False \
+    training.freeze=True \
+    training.frozen_epochs=20 \
+    training.frozen_batch_size=40 \
     training.batch_size=120 \
+    training.run_eval_at_launch=False \
+    training.run_vis_at_launch=False \
     losses.CaptionLoss.loss_wts.loss_caption=5e-2 \
     losses.VqaLoss.loss_wts.loss_vqa=1 \
     model.roi_head=False \
---
+++ exp/gpv_biatt_box_text/scripts/vqa_cap_det_diff_lr_quarter_data_freeze_then_ft.sh
@@ -0,0 +1,42 @@
+EXP_NAME=quarter_data_gpv_biatt_det_vqa_cap_wt_cap_5e-2_freeze_then_ft
+python -m exp.gpv_biatt_box_text.train_distr \
+    exp_name=$EXP_NAME \
+    ngpus_per_node=1 \
+    multiprocessing_distributed=True \
+    dist_url='tcp://localhost:10002' \
+    learning_datasets=det_vqa_cap \
+    training.ckpt=null \
+    training.freeze=True \
+    training.frozen_epochs=5 \
+    training.frozen_batch_size=120 \
+    training.batch_size=120 \
+    training.run_eval_at_launch=False \
+    training.run_vis_at_launch=False \
+    losses.CaptionLoss.loss_wts.loss_caption=5e-2 \
+    losses.VqaLoss.loss_wts.loss_vqa=1 \
+    model.roi_head=False \
+    model.detr_joiner.detr_dim=256 \
+    task_configs.coco_captioning.max_samples.train=73233 \
+    task_configs.coco_detection.max_samples.train=43634 \
+    task_configs.coco_vqa.max_samples.train=84597
+
+ckpt="/home/tanmayg/Data/gpv/coco_exp/${EXP_NAME}/ckpts/model.pth"
+python -m exp.gpv_biatt_box_text.train_distr \
+    exp_name=$EXP_NAME \
+    ngpus_per_node=2 \
+    multiprocessing_distributed=True \
+    dist_url='tcp://localhost:10002' \
+    learning_datasets=det_vqa_cap \
+    training.ckpt=$ckpt \
+    training.freeze=False \
+    training.batch_size=40 \
+    training.num_epochs=20 \
+    training.run_eval_at_launch=False \
+    training.run_vis_at_launch=False \
+    losses.CaptionLoss.loss_wts.loss_caption=5e-2 \
+    losses.VqaLoss.loss_wts.loss_vqa=1 \
+    model.roi_head=False \
+    model.detr_joiner.detr_dim=256 \
+    task_configs.coco_captioning.max_samples.train=73233 \
+    task_configs.coco_detection.max_samples.train=43634 \
+    task_configs.coco_vqa.max_samples.train=84597
