588291b11ffbe0d8fbaa4c52bdd279b2b643b674
Tanmay Gupta <tanmayg@allenai.org>
2021-02-02 22:04:13 +0000

    add script for 768 and add other detr ckpt names in setup_data.sh

--- setup_data.sh
+++ setup_data.sh
@@ -5,5 +5,6 @@ mkdir $GPV_DATA/learning_phase_data
 aws s3 cp $BUCKET/splits.zip $GPV_DATA/learning_phase_data/
 unzip $GPV_DATA/learning_phase_data/splits.zip -d $GPV_DATA/learning_phase_data/
 mkdir $GPV_DATA/detr
-aws s3 cp $BUCKET/detr/detr_gpv_checkpoint0299.pth $GPV_DATA/detr/detr_gpv_checkpoint0299.pth
+DETR_CKPT="detr_gpv_checkpoint0299.pth" #detr_gpv_coco_256_1e-4_bs_16.pth or detr_gpv_coco_768_5e-5_bs_32.pth
+aws s3 cp $BUCKET/detr/$DETR_CKPT $GPV_DATA/detr/$DETR_CKPT
 python -m data.coco.download download_coco_images_only=True
