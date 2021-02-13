08230b30e978327e6526201521afb850a354f415
Tanmay Gupta <tanmayg@allenai.org>
2021-02-13 15:21:48 -0800

    new split zip and download 2 detr models

--- setup_data.sh
+++ setup_data.sh
@@ -3,8 +3,8 @@ GPV_DATA=$HOME/Data/gpv
 mkdir -p $GPV_DATA
 mkdir $GPV_DATA/learning_phase_data
 aws s3 cp $BUCKET/splits.zip $GPV_DATA/learning_phase_data/
-unzip $GPV_DATA/learning_phase_data/splits.zip -d $GPV_DATA/learning_phase_data/
+unzip $GPV_DATA/learning_phase_data/original_and_gpv_splits.zip -d $GPV_DATA/learning_phase_data/
 mkdir $GPV_DATA/detr
-DETR_CKPT="detr_gpv_checkpoint0299.pth" #detr_gpv_coco_256_1e-4_bs_16.pth or detr_gpv_coco_768_5e-5_bs_32.pth
-aws s3 cp $BUCKET/detr/$DETR_CKPT $GPV_DATA/detr/$DETR_CKPT
+aws s3 cp $BUCKET/detr/detr-r50-e632da11.pth $GPV_DATA/detr/
+aws s3 cp $BUCKET/detr/detr_gpv_coco_256_1e-4_bs_16.pth $GPV_DATA/detr/
 python -m data.coco.download download_coco_images_only=True
