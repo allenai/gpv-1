0a0302e1e920f9141cd3665ed3bad0a830d02924
Tanmay Gupta <tanmayg@allenai.org>
2021-02-17 15:01:58 -0800

    add wo resizing detr

--- setup_data.sh
+++ setup_data.sh
@@ -5,6 +5,7 @@ mkdir $GPV_DATA/learning_phase_data
 aws s3 cp $BUCKET/original_and_gpv_splits.zip $GPV_DATA/learning_phase_data/
 unzip $GPV_DATA/learning_phase_data/original_and_gpv_splits.zip -d $GPV_DATA/learning_phase_data/
 mkdir $GPV_DATA/detr
+aws s3 cp $BUCKET/detr/detr_gpv_coco_wo_resizing.pth $GPV_DATA/detr/
 aws s3 cp $BUCKET/detr/detr-r50-e632da11.pth $GPV_DATA/detr/
 aws s3 cp $BUCKET/detr/detr_gpv_coco_256_1e-4_bs_16.pth $GPV_DATA/detr/
 python -m data.coco.download download_coco_images_only=True
