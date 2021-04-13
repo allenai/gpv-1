d24cbeeead409b5db23ee4e01808a552a0e1d92e
Tanmay Gupta <tanmayg@allenai.org>
2021-04-12 19:08:42 -0700

    add requirements and cleanup setup_conda_env

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -1,36 +1,14 @@
-#conda create -n gpv python=3.6 -y
-#conda activate gpv
-
 ################################################################################
 # conda installs
 ################################################################################
-conda install pytorch=1.6 torchvision cudatoolkit=10.2 -c pytorch -y
-conda install -c conda-forge scikit-image spacy spacy-lookups-data -y
-# conda install -c conda-forge scikit-image -y
-# conda install -c conda-forge spacy -y
-# conda install -c conda-forge spacy-lookups-data -y
-conda install h5py -y
-conda install -c cyclus java-jdk -y
+conda install -c pytorch pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.2.89 -y
+conda install -c conda-forge scikit-image=0.17.2 spacy=2.3.2 spacy-lookups-data=0.3.0 -y
+conda install -c cyclus java-jdk=8.45.14 -y
+conda install h5py=2.10.0 -y
 python -m spacy download en_core_web_sm
 
 
 ################################################################################
 # pip installs
 ################################################################################
-pip install hydra-core==1.0.0rc2
-pip install ipython
-pip install ipdb
-pip install tensorboard
-pip install opencv-python==4.2.0.32
-pip install tqdm
-pip install transformers
-pip install nltk
-pip install wget
-pip install PyDictionary
-pip install git+https://github.com/salaniz/pycocoevalcap
-pip install lmdb
-pip install git+https://github.com/ildoonet/pytorch-gradual-warmup-lr.git
-pip install boto3
-pip install tensorboardX
-pip install pytorch_transformers==1.0.0
-pip install imagesize
+pip install -r requirements.txt
