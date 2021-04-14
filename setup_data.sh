89031479b4d7b2949a636749725d2c3a0c26740f
Tanmay Gupta <tanmayg@allenai.org>
2021-04-14 11:47:11 -0700

    update setup_data to accept dir location

--- setup_data.sh
+++ setup_data.sh
@@ -1,11 +1,18 @@
-BUCKET="s3://ai2-prior-gpv"
-GPV_DATA=$HOME/Data/gpv
+BUCKET="s3://ai2-prior-gpv/public"
+
+# set to the location where you want to store downloaded data
+GPV_DATA=$1
+
+# download and extract coco and coco-sce splits
 mkdir -p $GPV_DATA
 mkdir $GPV_DATA/learning_phase_data
-aws s3 cp $BUCKET/original_and_gpv_splits.zip $GPV_DATA/learning_phase_data/
-unzip $GPV_DATA/learning_phase_data/original_and_gpv_splits.zip -d $GPV_DATA/learning_phase_data/
+aws s3 cp $BUCKET/coco_original_and_sce_splits.zip $GPV_DATA/learning_phase_data/
+unzip $GPV_DATA/learning_phase_data/coco_original_and_sce_splits.zip -d $GPV_DATA/learning_phase_data/
+
+# download detr pretrained on coco and coco-sce
 mkdir $GPV_DATA/detr
-aws s3 cp $BUCKET/detr/detr_gpv_coco_wo_resizing.pth $GPV_DATA/detr/
-aws s3 cp $BUCKET/detr/detr-r50-e632da11.pth $GPV_DATA/detr/
-aws s3 cp $BUCKET/detr/detr_gpv_coco_256_1e-4_bs_16.pth $GPV_DATA/detr/
-python -m data.coco.download download_coco_images_only=True
+aws s3 cp $BUCKET/detr/detr_coco.pth $GPV_DATA/detr/
+aws s3 cp $BUCKET/detr/detr_coco_sce.pth $GPV_DATA/detr/
+
+# Download coco images
+python -m data.coco.download download_coco_images_only=True output_dir=$GPV_DATA
