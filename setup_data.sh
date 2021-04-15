852a7bf45be6bc89efa162d56519c3b35139031d
Tanmay Gupta <tanmayg@allenai.org>
2021-04-15 13:08:03 -0700

    use public link and wget

--- setup_data.sh
+++ setup_data.sh
@@ -1,4 +1,4 @@
-BUCKET="s3://ai2-prior-gpv/public"
+BUCKET="https://ai2-prior-gpv.s3-us-west-2.amazonaws.com/public"
 
 # set to the location where you want to store downloaded data
 GPV_DATA=$1
@@ -6,13 +6,13 @@ GPV_DATA=$1
 # download and extract coco and coco-sce splits
 mkdir -p $GPV_DATA
 mkdir $GPV_DATA/learning_phase_data
-aws s3 cp $BUCKET/coco_original_and_sce_splits.zip $GPV_DATA/learning_phase_data/
+wget $BUCKET/coco_original_and_sce_splits.zip -P $GPV_DATA/learning_phase_data/
 unzip $GPV_DATA/learning_phase_data/coco_original_and_sce_splits.zip -d $GPV_DATA/learning_phase_data/
 
 # download detr pretrained on coco and coco-sce
 mkdir $GPV_DATA/detr
-aws s3 cp $BUCKET/detr/detr_coco.pth $GPV_DATA/detr/
-aws s3 cp $BUCKET/detr/detr_coco_sce.pth $GPV_DATA/detr/
+wget $BUCKET/detr/detr_coco.pth -P $GPV_DATA/detr/
+wget $BUCKET/detr/detr_coco_sce.pth -P $GPV_DATA/detr/
 
 # Download coco images
 python -m data.coco.download download_coco_images_only=True output_dir=$GPV_DATA
