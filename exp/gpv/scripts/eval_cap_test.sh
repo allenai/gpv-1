8215faa0109763d2a29f0867800fad1b85488cc7
Tanmay Gupta <tanmayg@allenai.org>
2021-05-20 16:43:40 -0700

    update eval_cap_test

--- exp/gpv/scripts/eval_cap_test.sh
+++ exp/gpv/scripts/eval_cap_test.sh
@@ -1,18 +1,18 @@
 EXP_NAME=$1
 SUBSET=$2   # possible values: {val, test}
+OUTPUT_DIR=$3
+DATA_DIR=$4
 
 echo "********************************************"
 echo "exp: ${EXP_NAME}"
 echo "split: coco (original_split)"
+echo "task: CocoCaptioning"
 echo "subset: ${SUBSET}"
 echo "********************************************"
 python -m exp.gpv.compute_cap_test_predictions \
     exp_name=$EXP_NAME \
+    output_dir=$OUTPUT_DIR \
+    data_dir=$DATA_DIR \
     task_configs.data_split=original_split \
-    model.max_text_len=20 \
     eval.subset=$SUBSET \
-    eval.predict=True \
-    eval.task=CocoCaptioning \
-    eval.num_eval_batches=null \
-    model.roi_head=True \
-    model.detr_joiner.detr_dim=2304
+    eval.task=CocoCaptioning
