977a6b3617d6f9c7a5750baf6740ecb84ee39fc1
Tanmay Gupta <tanmayg@allenai.org>
2021-05-20 18:11:13 -0700

    update eval_vqa_test

--- exp/gpv/scripts/eval_vqa_test.sh
+++ exp/gpv/scripts/eval_vqa_test.sh
@@ -1,17 +1,19 @@
 EXP_NAME=$1
 SUBSET=$2   # possible values: {test, testdev}
+OUTPUT_DIR=$3
+DATA_DIR=$4
 
 echo "********************************************"
 echo "exp: ${EXP_NAME}"
 echo "split: coco (original_split)"
+echo "task: CocoVqa"
 echo "subset: ${SUBSET}"
 echo "********************************************"
 python -m exp.gpv.compute_vqa_test_predictions \
     exp_name=$EXP_NAME \
+    output_dir=$OUTPUT_DIR \
+    data_dir=$DATA_DIR \
     task_configs.data_split=original_split \
     model.max_text_len=5 \
     eval.subset=$SUBSET \
-    eval.predict=True \
-    eval.num_eval_batches=null \
-    model.roi_head=True \
-    model.detr_joiner.detr_dim=2304
+    eval.task=CocoVqa
